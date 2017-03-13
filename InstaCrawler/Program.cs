1e5e07a396ae5d6938fb183d5f3c1f38372eed30
DESKTOP-UTT8HFR\dayar_o52hdk4 <dayarushin@gmail.com>
2017-03-13 21:44:50 +0500

    insta

---
+++ InstaCrawler/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace InstaCrawler
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+            Console.Write("Tag: #");
+            var tag = Console.ReadLine();
+
+            Console.Write("Count: ");
+            var count = int.Parse(Console.ReadLine());
+
+            var crawler = new Crawler();
+            crawler.TotalPhotoProcessedChanged += Crawler_TotalPhotoProcessedChanged;
+            
+            Console.WriteLine($"Поиск {count} фото с тегом #{tag}");
+            var photos = crawler.Start(tag, count);
+            var json = JsonConvert.SerializeObject(photos);
+            SaveData("Geotager\\photos.json",json);
+            Process.Start("Geotager\\index.html");
+        }
+
+        private static void Crawler_TotalPhotoProcessedChanged(int total, int withGeo)
+        {
+            Console.Write($"\rОбработано фото {total}. Геометка найдена: {withGeo}");
+        }
+
+        private static void SaveData(string path, string data)
+        {
+            File.WriteAllText(path,"var data = "+data+";");
+        }
+    }
+}
